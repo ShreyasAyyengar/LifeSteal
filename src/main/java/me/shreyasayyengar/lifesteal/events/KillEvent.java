c97a8f822c0fb2a7c4ae75be25dcdae71f06fa17
Shreyas A <shreyas.ayyengar@gmail.com>
2021-07-20 09:29:08 +0800

    Changed EntityKillByEntityEvent to PlayerDeathEvent, as that gives access to the killer

--- src/main/java/me/shreyasayyengar/lifesteal/events/KillEvent.java
+++ src/main/java/me/shreyasayyengar/lifesteal/events/KillEvent.java
@@ -4,37 +4,33 @@ import me.shreyasayyengar.lifesteal.config.Config;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.ChatColor;
-import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class KillEvent implements Listener {
 
     @EventHandler
-    private void onDeath(EntityDamageByEntityEvent e) {
+    private void onPlayerDeath(PlayerDeathEvent e) {
+        Player player = e.getEntity().getPlayer();
 
-        if (e.getDamager() instanceof Player damager && e.getEntity() instanceof Player victim) {
+        if (player.getKiller() != null) {
+            Player killer = player.getKiller();
 
-            if (damager.getGameMode() == GameMode.SURVIVAL && victim.getGameMode() == GameMode.SURVIVAL) {
+            if (killer.getHealthScale() < 20.0) {
 
-                if (damager.getHealthScale() < 20.0) {
 
-                    if (victim.getHealth() - e.getFinalDamage() <= 0) {
-
-                        damager.setHealthScaled(true);
-                        damager.setHealthScale(damager.getHealthScale() + 2);
-                        if (Config.reduceInternalHealth()) {
-                            //noinspection deprecation
-                            damager.setMaxHealth(damager.getHealthScale());
-                        }
-                        if (Config.shouldDisplayActionBar()) {
-                            //noinspection deprecation
-                            damager.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.GREEN +
-                                    "You gained a full heart by killing " + victim.getName()));
-                        }
-                    }
+                killer.setHealthScaled(true);
+                killer.setHealthScale(killer.getHealthScale() + 2);
+                if (Config.reduceInternalHealth()) {
+                    //noinspection deprecation
+                    killer.setMaxHealth(killer.getHealthScale());
+                }
+                if (Config.shouldDisplayActionBar()) {
+                    //noinspection deprecation
+                    killer.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.GREEN +
+                            "You gained a full heart by killing " + player.getName()));
                 }
             }
         }
